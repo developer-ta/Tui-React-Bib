3efd035212a5b32a7165b74403217e6a41d1b83b
tayier <ntparis9@gmail.com>
2025-04-08 01:37:01 +0200

    first commit

---
+++ src/component/TuiReactModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TuiReactModal = () => {
+	return (
+		<div>
+			<button type="button">click me</button>
+		</div>
+	);
+}
+
+export default TuiReactModal;
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+// vite.config.js  
+import { defineConfig } from 'vite';  
+import react from '@vitejs/plugin-react';  
+
+export default defineConfig({  
+  plugins: [react()],  
+  build: {  
+    lib: {  
+      entry: 'src/presentation_Layer/components/Tui-react-Bib/index.jsx', // Assurez-vous que ce chemin est correct  
+      name: 'TuiReactBib',  
+      fileName: (format) => `tui-react-bib.${format}.js`,  
+    },  
+    rollupOptions: {  
+      external: ['react', 'react-dom'],  
+      output: {  
+        globals: {  
+          react: 'React',  
+          'react-dom': 'ReactDOM',  
+        },  
+      },  
+    },  
+  },  
+});  
