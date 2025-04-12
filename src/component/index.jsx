274198d8cd239f659abfbfacc9adbc4e61fa46f5
tayier <ntparis9@gmail.com>
2025-04-12 14:58:28 +0200

    fixed bugs

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,1 +1,1 @@
-export { default as Modal } from './TuiReactModal';
+export { default as Modal } from './modal/TuiReactModal';
---
+++ src/component/modal/TuiReactModal.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import './TuiReactModal.scss';
+import { useMemo, useState } from 'react';
+const TuiReactModal = ({ visibility, message }) => {
+  const [hidden, setHidden] = useState('hidden');
+  const visible = useMemo(() => {
+    if (visibility) {
+      setHidden('');
+      return true;
+    }
+    setHidden('hidden');
+    return visibility;
+  }, [visibility]);
+  console.log('hidden: ', hidden);
+  console.log('visible: ', visible);
+  return (
+    <div className={`modal ${hidden}`}>
+      <div className="modal-container">
+        <h4 className="modal-detail">{message}</h4>
+        <button
+          onClick={() => {
+            if (visibility) setHidden('hidden');
+          }}
+          type="button"
+          className="modal-button modal-button-variant-1"
+        >
+          &#10005;
+        </button>
+      </div>
+    </div>
+  );
+};
+
+TuiReactModal.propTypes = {
+  visibility: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+};
+export default TuiReactModal;
